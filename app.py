b1c440950bb67dfa8cb41c706671cc2b5bbbe1ac
Rishav <rishavkumar79@gmail.com>
2021-06-19 00:40:02 +0530

    Login Page added

--- app.py
+++ app.py
@@ -61,8 +61,21 @@ app = Flask(__name__,template_folder='template')
 model = pickle.load(open('model_train.pkl', 'rb'))
 
 @app.route('/')
-def home():
-    return render_template('book_index.html')
+def hello_world():
+    return render_template("login.html")
+database={'nachi':'123','james':'aac','karthik':'asdsf'}
+
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	         return render_template('book_index.html',name=name1)
 
 @app.route('/predict',methods=['POST'])
 def predict():
